775d835f730ecaef157b983149999a48de5ea863
ikumiia <93059398+ikumiia@users.noreply.github.com>
2024-12-06 02:36:36 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String args[]) {
-        System.out.println("C++ is a general-purpose programming language with a bias towards systems programming that\n  - is a better C\n  - supports data abstraction\n  - supports object-oriented programming\n  - supports generic programming.");
-    }
-}
