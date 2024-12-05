0ff42f95d2ccdda0461875fdafa8fb1d28b921d6
ikumiia <93059398+ikumiia@users.noreply.github.com>
2024-12-06 02:35:16 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("C++ is a general-purpose programming language with a bias towards systems programming that\n  - is a better C\n  - supports data abstraction\n  - supports object-oriented programming\n  - supports generic programming.");
+    }
+}
